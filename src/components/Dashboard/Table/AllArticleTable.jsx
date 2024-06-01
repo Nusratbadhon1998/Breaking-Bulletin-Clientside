8d0315810d033e751cfd99dc190aab73b1dea207
Nusrat Badhon <njbworks121998@gmail.com>
2024-06-02 03:38:43 +0600

    Admin manage article page added

--- src/components/Dashboard/Table/AllArticleTable.jsx
+++ src/components/Dashboard/Table/AllArticleTable.jsx
@@ -1,42 +1,81 @@
-import React from "react";
+function AllArticleTable({ article, handleUpdate, handleDelete,handleModal }) {
+  const {
+    authorImage,
+    authorName,
+    authorEmail,
+    postedDate,
+    publisher,
+    status,
+    title,
+    premium,
+    _id,
+  } = article;
 
-function AllArticleTable() {
+
+  const date = new Date(postedDate).toLocaleDateString("en-US");
+
   return (
     <tr>
-      <th>
-        <label>
-          <input type="checkbox" className="checkbox" />
-        </label>
-      </th>
       <td>
         <div className="flex items-center gap-3">
           <div className="avatar">
-            <div className="mask mask-squircle w-12 h-12">
-              <img
-                src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png"
-                alt="Avatar Tailwind CSS Component"
-              />
+            <div className="mask w-12 h-12">
+              <img src={authorImage} alt="Avatar Tailwind CSS Component" />
             </div>
-          </div>
-          <div>
-            <div className="font-bold">Hart Hagerty</div>
-            <div className="text-sm opacity-50">United States</div>
           </div>
         </div>
       </td>
+      <td>{authorName}</td>
+      <td>{authorEmail}</td>
+      <td>{title}</td>
+      <td>{date}</td>
       <td>
-        Zemlak, Daniel and Leannon
-        <br />
-        <span className="badge badge-ghost badge-sm">
-          Desktop Support Technician
+        <span
+          className={`${
+            status === "Pending" ? "text-yellow-500 bg-yellow-200 p-2" : ""
+          } ${status === "Approved" ? "text-lime-500 bg-lime-200 p-2" : ""} ${
+            status === "Declined" ? "text-red-500 bg-stone-200 p-2" : ""
+          }`}
+        >
+          {status}
         </span>
       </td>
-      <td>Purple</td>
+      <td>{publisher}</td>
       <th>
-        <button className="btn btn-ghost btn-xs">details</button>
+        <button
+          onClick={() => handleUpdate({ id: _id, status: "Approved" })}
+          className="btn btn-ghost btn-xs"
+        >
+          Approve
+        </button>
+      </th>
+      <th>
+        <button
+          onClick={() => {
+            handleUpdate({ id: _id, status: "Declined" });
+            handleModal(_id)
+          }}
+          className="btn btn-ghost btn-xs"
+        >
+          Decline
+        </button>
+      </th>
+      <th>
+        <button className="btn btn-ghost btn-xs">Delete</button>
+      </th>
+      <th>
+        {premium === "yes" ? (
+          "Premium"
+        ) : (
+          <button
+            onClick={() => handleUpdate({ id: _id, isPremium: "yes" })}
+            className="btn btn-ghost btn-xs"
+          >
+            Make Premium
+          </button>
+        )}
       </th>
     </tr>
   );
 }
-
 export default AllArticleTable;
--- src/pages/Dashboard/AllArticles/AllArticles.jsx
+++ src/pages/Dashboard/AllArticles/AllArticles.jsx
@@ -1,47 +1,136 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import AllArticleTable from "../../../components/Dashboard/Table/AllArticleTable";
+import { toast } from "react-toastify";
+import ArticleTableRow from "../../../components/Dashboard/Table/ArticleTableRow";
+import { useState } from "react";
+import DeclineDialogue from "../../../components/Dashboard/Dialogue/DeclineDialogue";
 
 function AllArticles() {
   const { user, loading } = useAuth();
+  let [isOpen, setIsOpen] = useState(false);
+  const [articleId, setArticleId] = useState("");
   const axiosSecure = useAxiosSecure();
-
-  const { data: articles = [], isLoading } = useQuery({
+  // get all articles
+  const {
+    data: articles = [],
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["articles"],
+    enabled: !loading && !!user,
     queryFn: async () => {
       const { data } = await axiosSecure.get("/articles");
       return data;
     },
   });
+
+  //   add decline reason
+  const { mutateAsync } = useMutation({
+    mutationFn: async (reason) => {
+      const { data } = await axiosSecure.put(`/articles/${articleId}`, {
+        declineReason: reason,
+      });
+      console.log(data);
+    },
+    onSuccess:()=>{
+        console.log("success")
+    }
+  });
+  // Change status and premium
+  const handleUpdate = async ({ id, status, isPremium }) => {
+    if (isPremium) {
+      try {
+        const { data } = await axiosSecure.patch(`/articles/${id}`, {
+          isPremium,
+        });
+        if (data.modifiedCount) {
+          toast.success("Updated premium");
+          refetch();
+        }
+      } catch (error) {
+        console.log("Error in handleStatus:", error); // Debugging line
+        toast.warning(error.message);
+      }
+    }
+    if (status) {
+      try {
+        const { data } = await axiosSecure.patch(`/articles/${id}`, {
+          status,
+        });
+        if (data.modifiedCount) {
+          toast.success("Updated");
+          refetch();
+        }
+      } catch (error) {
+        console.log("Error in handleStatus:", error); // Debugging line
+        toast.warning(error.message);
+      }
+    }
+  };
+
+  const handleModal = (_id) => {
+    setIsOpen(true);
+    setArticleId(_id);
+  };
+
+  const handleDeclineReason = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const declineReason = form.reason.value;
+    console.log(declineReason);
+
+    mutateAsync(declineReason);
+
+    setIsOpen(false);
+  };
+
+  const handleDelete = async (id) => {
+    console.log(id);
+  };
+
   return (
-    <section>
+    <section className="my-20 mx-auto">
       <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
-          <thead>
+          <thead className="text-stone-800">
             <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <th>Author Photo</th>
+              <th>Author Name</th>
+              <th>Author email</th>
+              <th>Title</th>
+              <th>Posted Date</th>
+              <th>Status</th>
+              <th>Publisher</th>
+              <th></th>
+              <th></th>
+              <th></th>
               <th></th>
             </tr>
           </thead>
           <tbody>
-            {/* row 1 */}
-
             {articles.map((article) => (
-              <AllArticleTable article={article} key={article._id} />
+              <AllArticleTable
+                handleDelete={handleDelete}
+                handleUpdate={handleUpdate}
+                handleModal={handleModal}
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+                article={article}
+                key={article._id}
+              />
             ))}
           </tbody>
         </table>
       </div>
+      <DeclineDialogue
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        handleDeclineReason={handleDeclineReason}
+      />
     </section>
   );
 }
